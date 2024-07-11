222143f8aa5ce8223ad2b2576199eb4d2569e9f2
Bene-Tesla <benevaniosantos930@gmail.com>
2024-07-11 14:11:25 -0300

    feat: add anotations for find user by id

--- src/main/java/com/bkorp/course/resources/UserResource.java
+++ src/main/java/com/bkorp/course/resources/UserResource.java
@@ -1,23 +1,34 @@
 package com.bkorp.course.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bkorp.course.entities.User;
+import com.bkorp.course.services.UserServices;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+
+    @Autowired
+    private UserServices service;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        List<User> list = new ArrayList<>();
-        list.add(new User(1L, "Maria Brown", "Maria@gmail.com", "99999999", "123456"));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
