d459100faf59327b53524e8e18252e6c0e92e120
Bene-Tesla <benevaniosantos930@gmail.com>
2024-07-11 22:08:07 -0300

    feat: add Category.java

---
+++ src/main/java/com/bkorp/course/entities/Category.java
@@ -0,0 +1,59 @@
+package com.bkorp.course.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // hashcode by id
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    // equals by id
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Category category = (Category) obj;
+        return id.equals(category.id);
+    }
+}
